f9353dab474101b3ddbde4e683fb298b3f3e8b41
jwmccowan <jwmccowan@gmail.com>
2019-09-12 14:10:30 +1000

    #3 actions
    added a filterPokemonsAction which just passes the search term to the reducer, and raised the limit of pokemon that we ask for in fetchPokemonsAction so that we have enough pokemon to filter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,19 @@
 import pokemonAPI from '../apis/pokemon';
 
 export const fetchPokemonsAction = () => async (dispatch) => {
-  const response = await pokemonAPI.get('/pokemon');
+  const response = await pokemonAPI.get('/pokemon',
+    {
+      params: {
+        limit: 151,
+      },
+    });
 
   dispatch({ type: 'FETCH_POKEMONS', payload: response.data.results });
 };
 
-export const filterPokemons = () => 'blah';
+export const filterPokemonsAction = (term) => (
+  {
+    type: 'FILTER_POKEMONS',
+    payload: { term },
+  }
+);
